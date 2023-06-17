fde314304f28670873aef36b6d625c5f622bb8f9
Srujana Madiraju <123050301+srujanamadiraju@users.noreply.github.com>
2023-06-17 19:45:26 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,31 +5,35 @@ namespace student_proj
   {
     static void Main(string [] args)
     {  
-      //PROGRAM 1
-       Console.WriteLine("enter number:");
-         int n = Convert.ToInt32(Console.ReadLine());
-         int fact=1;
-         int flag =0;
-         for (int i=n; i>0;i--)
-         {
-          fact =fact*i;
-         }
-         Console.WriteLine("the factorial is " +fact);
-         // to find the number of trailing zeroes 
-         while(fact!=0)
-         {
-              if(fact%10==0)
-              {
-                flag++;
-                fact=fact/10;
-              }
-              else 
-              {
-                break;
-              }
-         }
-         Console.WriteLine("the number of zeroes in the factorial number is: "+ flag);
+      //PROGRAM 2
+      Console.WriteLine("enter the no of matches :");
+      int Y = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("enter the no of points:");
+      int X = Convert.ToInt32(Console.ReadLine());
+      int count;
+      // cases available:
+      string possibility= " "; 
+      switch(possibility)
+      {
+        case "Win":
+        count =2;
+        break;
+        case "Tie":
+        count =1;
+        break;
+        case "lose":
+        count=0;
+        break;
+        default:
+        count=-1;
+        break; 
+      }
+      
+      int average ;
+      average = (X+Y)/2;
+      Console.WriteLine(average);
 
+       
 
 
      
